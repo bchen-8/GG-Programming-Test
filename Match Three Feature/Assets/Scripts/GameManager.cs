85861088c9baecc0724081865f260f0ea6a2be76
Chen <bchen8@student.gsu.edu>
2021-05-20 13:07:57 -0400

    Basic Victory Condition Implemented
    - PrizeRoll() increments the corresponding value in prizeCount[] by 1
    - CheckVictory() tracks if any prize has appeared three or more times, and uses a switch case to determine the resulting victory screen if the condition is met
    - If the condition is met, a VictoryScreen gameobject is set to active, with its text corresponding to the type of victory
    - Closed boxes are still openable after victory is attained.

--- Match Three Feature/Assets/Scripts/GameManager.cs
+++ Match Three Feature/Assets/Scripts/GameManager.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     GameManager gameManager;
 
+    //List of prefabs, references set up in the Editor
     public GameObject closedTile;
     public GameObject miniTile;
     public GameObject minorTile;
     public GameObject maxiTile;
     public GameObject majorTile;
     public GameObject grandTile;
+
+    public GameObject victoryScreen;
+    public TextMeshPro victoryText;
 
     int[] prizeCount = new int[5];
     List<GameObject> boardList = new List<GameObject>();
@@ -41,12 +46,7 @@ public class GameManager : MonoBehaviour
         */
     }
 
-    void Update()
-    {
-        
-    }
-
-    #region Instantiation
+    #region Scene Setup
     private void GenerateGrid() //Spawn 3x5 grid of closed boxes, track with a list
     {
         int count = 0;
@@ -67,14 +67,14 @@ public class GameManager : MonoBehaviour
     #endregion
 
     #region Click Register
-    public void OpenBox(GameObject obj)
+    public void OpenBox(GameObject obj) //Player input tracked by OnMouseDown() in ClosedTileManager
     {
         ClosedTileManager objScript = obj.GetComponent<ClosedTileManager>();
         int objIndex = objScript.listIndex;
-        //Debug.Log(objIndex);
 
-        GameObject prize = PrizeRoll();
+        GameObject prize = PrizeRoll(); //Receive results of PrizeRoll()
         objScript.RevealPrize(prize);
+        CheckVictory();
     }
 
     public void SetPrizeInList(GameObject obj, int index)
@@ -82,25 +82,30 @@ public class GameManager : MonoBehaviour
         boardList[index] = obj;
     }
 
-    private GameObject PrizeRoll() //Rolls the probability and returns the appropriate GameObject
+    private GameObject PrizeRoll() //Rolls the probability, increments victory condition array, and returns the appropriate GameObject.
     {
         int roll = Random.Range(1, 101);
         Debug.Log("PrizeRoll(): Rolled " + roll);
 
         if (roll <= 50) {
             Debug.Log("PrizeRoll(): Replacing tile with a Mini Tile");
+            prizeCount[0]++;
             return miniTile;
         } else if (roll > 50 && roll <= 75) {
             Debug.Log("PrizeRoll(): Replacing tile with a Minor Tile");
+            prizeCount[1]++;
             return minorTile;
         } else if (roll > 75 && roll <= 90) {
             Debug.Log("PrizeRoll(): Replacing tile with a Maxi Tile");
+            prizeCount[2]++;
             return maxiTile;
         } else if (roll > 90 && roll <= 98) {
             Debug.Log("PrizeRoll(): Replacing tile with a Major Tile");
+            prizeCount[3]++;
             return majorTile;
         } else if (roll > 98 && roll <= 100) {
             Debug.Log("PrizeRoll(): Replacing tile with a Grand Tile");
+            prizeCount[4]++;
             return grandTile;
         } else {
             Debug.Log("<color=red> PrizeRoll(): Number rolled fell outside of bounds. </color>");
@@ -112,12 +117,36 @@ public class GameManager : MonoBehaviour
     #region Victory Condition
     private void CheckVictory() //Check for the Victory condition
     {
-
+        for (int i = 0; i <= 4; i++) {
+            if (prizeCount[i] >= 3) {
+                switch (i) {
+                    case 0:
+                        VictoryCondition("Mini Prize!");
+                        break;
+                    case 1:
+                        VictoryCondition("Minor Prize!");
+                        break;
+                    case 2:
+                        VictoryCondition("Maxi Prize!");
+                        break;
+                    case 3:
+                        VictoryCondition("Major Prize!");
+                        break;
+                    case 4:
+                        VictoryCondition("Grand Prize!");
+                        break;
+                    default:
+                        Debug.Log("<color=red> CheckVictory(): prizeCount index fell out of bounds. </color>");
+                        break;
+                }
+            }
+        }
     }
 
-    private void VictoryCondition()
+    private void VictoryCondition(string prizeText)
     {
-
+        victoryText.text = prizeText;
+        victoryScreen.SetActive(true);
     }
     #endregion
 }
